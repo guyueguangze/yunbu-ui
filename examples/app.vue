adc00f25064ffeb92ffe4836aa2cd4ee65ef9e30
胡光泽 <2581504687@qq.com>
2022-11-16 15:35:47 +0800

    .

---
+++ examples/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+     5555 
+       <Button></Button>
+    </div>
+</template>
+<script lang="ts" setup>
+import {Button} from 'yunbu-ui'
+</script>
---
+++ packages/components/src/button/index.ts
@@ -0,0 +1,5 @@
+import button from './button.vue'
+import {withInstall} from '@yunbu-ui/utils'
+const Button = withInstall(button)
+export default Button
+
